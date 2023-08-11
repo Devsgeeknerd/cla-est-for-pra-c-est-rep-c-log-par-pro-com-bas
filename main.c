b9889586ff0ff2fa7f7e3f33f8b8890cb08f6179
Paulo Vanderley <vanderley.1109@gmail.com>
2023-08-11 11:08:15 -0300

    adicionando conteudo

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero = 0;
+    printf("Digite o numero: ");
+    scanf("%d", &numero);
+    int = 0;
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d + %d = %d\n", numero, i, numero + i);
+        printf("%d - %d = %d\n", numero, i, numero - i);
+        printf("%d x %d = %d\n", numero, i, numero * i);
+        printf("%d / %d = %d\n", numero, i, numero / i);
+    }
+    return 0;
+}
